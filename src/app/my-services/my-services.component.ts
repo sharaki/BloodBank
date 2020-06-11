903f2f221c3ea7d3c7f520901876c32fa10c87bf
gehad kassap <gohadkasaa@gmail.com>
2020-06-11 17:02:17 +0200

    doner/reciever buttons

--- src/app/my-services/my-services.component.ts
+++ src/app/my-services/my-services.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-my-services',
   templateUrl: './my-services.component.html',
   styleUrls: ['./my-services.component.scss']
 })
+
+
 export class MyServicesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    /********** to close modal donate****** */
+    $("#closeDonner").click(function(){
+      $(".donner").css("display","none")
+
+    })
+    /********** to open modal donate************ */
+    $("#btnDonate").click(function(){
+      $(".donner").css("display","block")
+
+    })
+
+     /********** to open moda recieve****** */
+
+     $("#btnRecieve").click(function(){
+      $(".userRecieve").css("display","block")
+
+
+    })
+    /********** to close modal recieve************ */
+
+    $("#closeRecieve").click(function(){
+      $("#userRecieve").css("display","none")
+
+    })
+
+
   }
 
 }
