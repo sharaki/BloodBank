9fa3ab54d05fbecb78962d639254138cb357fcad
Omar ElHawary <omaraelhawary@gmail.com>
2020-06-12 18:00:24 +0200

    update events cards

--- src/app/camps/camps.component.ts
+++ src/app/camps/camps.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-camps',
   templateUrl: './camps.component.html',
@@ -8,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class CampsComponent implements OnInit {
   
   constructor() { }
+  myMethod(){
+    
+  }
+  ngOnInit() {
+    $(document).ready(function() {
+  
+      var stack1 = $('#stack1');
+      
+      stack1.removeClass('start');
+      
+      stack1.hammer().on('tap', function(event) {
+        stack1.addClass('hover');
+        event.stopPropagation();
+      });
+      
+      
+      $(document).hammer().on('tap', function(event) {
+        stack1.removeClass('hover');
+        $('.card').removeClass('hover');
+      });
+      
+      $('.card').hammer().on('tap', function(event) {
+          $('.card').removeClass('hover');
+          $(this).addClass('hover');
+      });
+    });
 
-  ngOnInit(): void {
     
       
   }
-
- 
 }
