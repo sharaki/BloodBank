b3cbcb50c8969091a9c3ed681d9cbb6c750b7df5
Mahmoud Sharaki <m.sharaki@hotmail.com>
2020-06-15 15:05:45 +0200

    Revert "our sponsor div - heading of all sections"

--- src/app/camps/camps.component.ts
+++ src/app/camps/camps.component.ts
@@ -83,18 +83,8 @@ export class CampsComponent implements OnInit {
         }
       });
       
-      });
-      $('.bxslider').bxSlider({
-        autoHover: true,
-        auto: true,
-        slideWidth: 250,
-        minSlides: 2,
-        maxSlides: 6,
-        controls: true,
-        pager: true,
-        speed: 500,
-        captions: true,
-        slideMargin: 5,
+      
+      
       });
     
   }
