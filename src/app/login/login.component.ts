191c59fdfaf1d357f719c6dbfb0b695ffd7e389e
Mahmoud Sharaki <m.sharaki@hotmail.com>
2020-06-15 20:56:20 +0200

    Revert "Gehad"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +8,6 @@ import { FormGroup , FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
-  signIn = new FormGroup({
-
-    email : new FormControl('' , [Validators.required , Validators.email]),
-
-    password: new FormControl('', Validators.required)
-  })
-
-  FormSignIn()
-  {
-    console.log(this.signIn);
-  }
 
   ngOnInit(): void {
 
