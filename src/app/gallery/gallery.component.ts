40d8033651f4b8063d8655bbe8af8cc6640bbf0d
Omar ElHawary <omaraelhawary@gmail.com>
2020-06-15 14:27:11 +0200

    our sponsor div - heading of all sections

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-gallery',
@@ -10,6 +11,9 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    
+    
   }
 
 }
